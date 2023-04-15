f70407a49639bedb0dfbfa245256becaa0828f79
sawasawashu <sawasawa01030420@gmail.com>
2023-04-15 16:14:38 +0900

    ユーザー管理機能の実装完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+  validates :last_name_kana, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+  validates :first_name_kana, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
   validates :birthday, presence: true
 
 end
