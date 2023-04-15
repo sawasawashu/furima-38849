707411d075a17e8910dc10678b081d636e885ad4
sawasawashu <sawasawa01030420@gmail.com>
2023-04-15 23:55:29 +0900

    FactoryBotにユーザーの値を定義

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname {Faker::Name.nickname}
+    email {Faker::Internet.free_email}
+    password {Faker::Alphanumeric.alpha_numberic(6)}
+    password_confirmation {password}
+    last_name {Faker::Name.last_name}
+    first_name {Faker::Name.first_name}
+    last_name_kana {Faker::Name.last_name_kana}
+    first_name_kana {Faker::Name.first_name_kana}
+    birthday {Birthday.all.sample}
   end
 end
