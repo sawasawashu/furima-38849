9945cacabe513521466d13b1cd58e024bc623fe5
sawasawashu <sawasawa01030420@gmail.com>
2023-04-19 17:02:46 +0900

    ログイン状態のみ出品ページへ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,20 @@
 class ItemsController < ApplicationController
+
+  before_action :move_to_login, except: :index
+
   def index
   end
 
   def new
     @item = Item.new
   end
-  
+
+  private
+
+  def move_to_login
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
+
 end
