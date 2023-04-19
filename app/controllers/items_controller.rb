a29224fabf1fb1a8a5b7d8a542d6f7c103beb0a1
sawasawashu <sawasawa01030420@gmail.com>
2023-04-20 00:14:38 +0900

    商品データをデータベースへ保存まで実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
 
-  before_action :move_to_login, except: :index
+  before_action :move_to_login, only: :new
 
   def index
   end
@@ -9,7 +9,20 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   private
+
+  def item_params
+    params.require(:item).permit(:product_name,:introduction,:category_id,:status_id,:charge_id,:region_id,:delivery_day_id,:price,:image).merge(user_id: current_user.id)
+  end
 
   def move_to_login
     unless user_signed_in?
