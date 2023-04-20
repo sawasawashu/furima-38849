3012fba938b9de64acab5be542df7407e3a7749d
sawasawashu <sawasawa01030420@gmail.com>
2023-04-20 17:40:44 +0900

    商品を出品できる場合のテストコード

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe Item, type: :model do
   before do
     @item = FactoryBot.build(:item)
   end
+
+  describe '商品の出品' do
+    context '商品を出品できる' do
+      it "image,product_name,introduction,category_id,status_id,charge_id,region_id,delivery_day_id,priceの値が存在すれば出品できる" do
+       expect(@item).to be_valid
+      end 
+    end
+  end
 end
