ba06a32ffa6044f94a7ff7075e59e87c377aab76
sawasawashu <sawasawa01030420@gmail.com>
2023-04-20 15:53:34 +0900

    テスト用の出品モデル作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    image           { "test_image.png"}
+    product_name    { Faker::Name.name }
+    introduction    { Faker::Lorem.sentence }
+    category_id     { 2}
+    status_id       { 2}
+    charge_id       { 2}
+    region_id       { 2}
+    delivery_day_id { 2}
+    price           { 10000 }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
