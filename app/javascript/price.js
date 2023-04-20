b6148698d8f7035f94dcc0c862fe5fca4f431d82
sawasawashu <sawasawa01030420@gmail.com>
2023-04-20 14:03:10 +0900

    販売手数料・利益の自動計算

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -3,5 +3,12 @@ window.addEventListener('load', () => {
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
     console.log(inputValue);
+    const tax = Math.floor(inputValue * 0.1) ;
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = tax;
+    const profit = Math.floor(inputValue - tax);
+    const margin = document.getElementById("profit");
+    margin.innerHTML = profit;  
+
   })
 })
