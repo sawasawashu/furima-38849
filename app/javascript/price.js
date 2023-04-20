74bdd92799b4f99696c1c4d066b86924fff440f3
sawasawashu <sawasawa01030420@gmail.com>
2023-04-20 12:20:40 +0900

    入力した金額の取得

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,3 +1,7 @@
 window.addEventListener('load', () => {
-  console.log("OK");
-});
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    console.log(inputValue);
+  })
+})
