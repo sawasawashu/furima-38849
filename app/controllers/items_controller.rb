9402930397aca5dbbbde469e17055fa0ddcfcb5f
sawasawashu <sawasawa01030420@gmail.com>
2023-04-26 15:31:05 +0900

    show,edit,updateアクションをまとめて記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :move_to_login, only: :new
   before_action :authenticate_user!, only: :edit
   before_action :move_to_index, only: :edit
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -21,15 +22,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path, method: :get
     else
@@ -55,5 +53,9 @@ class ItemsController < ApplicationController
     return if current_user == @item.user
 
     redirect_to action: :index
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
