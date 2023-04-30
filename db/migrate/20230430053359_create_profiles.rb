3be32cd95cf3dbbdde3f441b11e908d19cfb51bb
sawasawashu <sawasawa01030420@gmail.com>
2023-04-30 14:57:08 +0900

    購入者情報のテーブル作成

--- db/migrate/20230430053359_create_profiles.rb
+++ db/migrate/20230430053359_create_profiles.rb
@@ -1,7 +1,13 @@
 class CreateProfiles < ActiveRecord::Migration[6.0]
   def change
     create_table :profiles do |t|
-
+      t.string      :postal_code,      null: false 
+      t.integer     :region_id,        null: false
+      t.string      :municipality,     null: false
+      t.string      :address,          null: false
+      t.string      :building_name
+      t.string      :telephone_number, null: false
+      t.references  :purchaser,        null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -49,8 +49,16 @@ ActiveRecord::Schema.define(version: 2023_04_30_053359) do
   end
 
   create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "postal_code", null: false
+    t.integer "region_id", null: false
+    t.string "municipality", null: false
+    t.string "address", null: false
+    t.string "building_name"
+    t.string "telephone_number", null: false
+    t.bigint "purchaser_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["purchaser_id"], name: "index_profiles_on_purchaser_id"
   end
 
   create_table "purchasers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -82,6 +90,7 @@ ActiveRecord::Schema.define(version: 2023_04_30_053359) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "items", "users"
+  add_foreign_key "profiles", "purchasers"
   add_foreign_key "purchasers", "items"
   add_foreign_key "purchasers", "users"
 end
