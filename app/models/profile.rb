0d5f5f1c67c7e08a81b79e2cae5ebca0c01168e9
sawasawashu <sawasawa01030420@gmail.com>
2023-04-30 15:04:46 +0900

    region_idのバリデーション設定

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,4 +4,6 @@ class Profile < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :region
 
+  validates :region_id, numericality: { other_than: 1, message: "can't be blank" }
+
 end
