b5bf0d94047da9d7ddf2f1eb51743c8a40ba724f
sawasawashu <sawasawa01030420@gmail.com>
2023-04-30 15:21:39 +0900

    profileバリデーション再設定

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,10 +4,9 @@ class Profile < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :region
 
-  postal_pattern = /\A\d{3}-\d{4}\z/
-  validates :postal_code, presence: true, format: {with: postal_pattern}
+  validates :postal_code, presence: true, format: {with: /\A\d{7}\z/}
   validates :region_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :municipality, presence: true
   validates :address, presence: true
-  validates :telephone_number, presence: true
+  validates :telephone_number, presence: true, format: { with: /\A\d{10,11}\z/ }
 end
