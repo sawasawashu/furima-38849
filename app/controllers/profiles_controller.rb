3291d3516bf44b5a492fbc76c0b20b6da6950b26
sawasawashu <sawasawa01030420@gmail.com>
2023-04-30 15:42:52 +0900

    アクションの内容を記述

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,9 +1,23 @@
 class ProfilesController < ApplicationController
  
   def index
+    @profile = Profile.new 
   end
 
   def create
+    @profile = Profile.new(profile_params)
+    if @profile.valid?
+      @profile.save
+      return redirect_to root_path
+    else
+      render :index
+    end
   end
-  
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:postal_code, :region_id, :municipality, :address, :building_name, :telephone_number)
+  end
+
 end
