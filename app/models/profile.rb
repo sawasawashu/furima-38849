b5ecd20b5d0251b21d7737c63aa7715897e824d4
sawasawashu <sawasawa01030420@gmail.com>
2023-05-01 17:02:36 +0900

    Formオブジェクトモデルにバリデーション

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,9 +4,4 @@ class Profile < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :region
 
-  validates :postal_code, presence: true, format: {with: /\A\d{7}\z/}
-  validates :region_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :municipality, presence: true
-  validates :address, presence: true
-  validates :telephone_number, presence: true, format: { with: /\A\d{10,11}\z/ }
 end
--- app/models/purchaser_profile.rb
+++ app/models/purchaser_profile.rb
@@ -1,4 +1,14 @@
 class PurchaserProfile
   include ActiveModel::Model
-  attr_accessor :postal_code, :region_id, :municipality, :address, :telephone_number, :user_id, :item_id
+  attr_accessor :postal_code, :region_id, :municipality, :address, :building_name, :telephone_number, :user_id, :item_id
+
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A\d{7}\z/}
+    validates :municipality
+    validates :address
+    validates :telephone_number, format: { with: /\A\d{10,11}\z/ }
+    validates :user_id
+    validates :item_id
+  end
+  validates :region_id, numericality: { other_than: 1, message: "can't be blank" }
 end
