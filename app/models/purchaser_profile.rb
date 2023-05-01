f0b30c884b5f143171c37e7191b5b1d4921ef2e0
sawasawashu <sawasawa01030420@gmail.com>
2023-05-01 18:38:20 +0900

    保存機能まで実装

--- app/models/purchaser_profile.rb
+++ app/models/purchaser_profile.rb
@@ -3,7 +3,7 @@ class PurchaserProfile
   attr_accessor :postal_code, :region_id, :municipality, :address, :building_name, :telephone_number, :user_id, :item_id
 
   with_options presence: true do
-    validates :postal_code, format: {with: /\A\d{7}\z/}
+    validates :postal_code, format: {with: /\A\d{3}[-]\d{4}\z/}
     validates :municipality
     validates :address
     validates :telephone_number, format: { with: /\A\d{10,11}\z/ }
@@ -15,7 +15,7 @@ class PurchaserProfile
   def save
     purchaser = Purchaser.create(user_id: user_id, item_id: item_id)
 
-    Profile.create(postal_code: postal_code, region_id: region_id, municipality: municipality,address: address, building_name: building_name, telephone_number: telephone_number, purchaser_id: purchaser_id)
+    Profile.create(postal_code: postal_code, region_id: region_id, municipality: municipality,address: address, building_name: building_name, telephone_number: telephone_number, purchaser: purchaser)
   end
   
 end
