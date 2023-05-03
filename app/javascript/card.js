aab2ed2f2293ba388411a78c8597bcdce11a6e29
sawasawashu <sawasawa01030420@gmail.com>
2023-05-03 18:43:40 +0900

    トークン化の処理

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,19 +1,26 @@
 const pay = () => {
-  const payjp = Payjp('pk_test_25ba193d18561a044400061f')// PAY.JPテスト公開鍵
-  const elements = payip.elements();
+  const payjp = Payjp('pk_test_25ba193d18561a044400061f')
+  const elements = payjp.elements();
   const numberElement = elements.create('cardNumber');
   const expiryElement = elements.create('cardExpiry');
   const cvcElement = elements.create('cardCvc');
 
-  numberElement.mount('#card-number')
-  expiryElement.mount('#card-exp-month')
-  expiryElement.mount('#card-exp-year')
-  cvcElement.mount('#card-cvc')
+  numberElement.mount('#card-number');
+  expiryElement.mount('#card-exp-month');
+  expiryElement.mount('#card-exp-year');
+  cvcElement.mount('#card-cvc');
 
   const submit = document.getElementById("button");
+
   submit.addEventListener("click", (e) => {
     e.preventDefault();
-    console.log("フォーム送信時にイベント発火")
+    payjp.createToken(numberElement).then(function (response) {
+      if (response.error) {
+      } else {
+        const token = response.id;
+        console.log(token)
+      }
+    });
   });
 };
 
