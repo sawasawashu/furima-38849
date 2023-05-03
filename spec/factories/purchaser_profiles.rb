6526deb3195fccc58b8e14f70335c4b70e04dd22
sawasawashu <sawasawa01030420@gmail.com>
2023-05-03 09:52:29 +0900

    テストデータを生成

--- spec/factories/purchaser_profiles.rb
+++ spec/factories/purchaser_profiles.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :purchaser_profile do
-    
+    postal_code       {'123-6789'}
+    region_id         { 2 }
+    municipality      {'横浜市緑区'}
+    address           {'30-30'}
+    building_name     {'テストハウス'}
+    telephone_number  {08012341234}
+    association :user
+    association :item
   end
 end
