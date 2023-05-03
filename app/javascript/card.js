2b51ac67437d7ff646ccae67b8e7d31ecc9b2e19
sawasawashu <sawasawa01030420@gmail.com>
2023-05-03 16:12:29 +0900

    入力フォーム作成

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,15 @@
 const pay = () => {
   const payjp = Payjp('pk_test_25ba193d18561a044400061f')// PAY.JPテスト公開鍵
+  const elements = payip.elements();
+  const numberElement = elements.create('cardNumber');
+  const expiryElement = elements.create('cardExpiry');
+  const cvcElement = elements.create('cardCvc');
+
+  numberElement.mount('#card-number')
+  expiryElement.mount('#card-exp-month')
+  expiryElement.mount('#card-exp-year')
+  cvcElement.mount('#card-cvc')
+
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
