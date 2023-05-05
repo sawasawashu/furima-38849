92b65c53890f1fb2d175e6879573a371f41d1c81
sawasawashu <sawasawa01030420@gmail.com>
2023-05-05 16:23:26 +0900

    カード情報を削除

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -20,8 +20,10 @@ const pay = () => {
         const renderDom = document.getElementById("charge-form");
         const tokenObj = `<input value=${token} name='token' type="hidden">`;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
-        debugger;
       }
+      numberElement.clear();
+      expiryElement.clear();
+      cvcElement.clear();
     });
   });
 };
