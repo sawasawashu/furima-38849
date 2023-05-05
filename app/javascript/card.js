efa40cb42162040e48fd1def8512af2ce8d6e5e2
sawasawashu <sawasawa01030420@gmail.com>
2023-05-05 16:10:26 +0900

    トークンの値取得

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -5,10 +5,9 @@ const pay = () => {
   const expiryElement = elements.create('cardExpiry');
   const cvcElement = elements.create('cardCvc');
 
-  numberElement.mount('#card-number');
-  expiryElement.mount('#card-exp-month');
-  expiryElement.mount('#card-exp-year');
-  cvcElement.mount('#card-cvc');
+  numberElement.mount('#number-form');
+  expiryElement.mount('#expiry-form');
+  cvcElement.mount('#cvc-form');
 
   const submit = document.getElementById("button");
 
