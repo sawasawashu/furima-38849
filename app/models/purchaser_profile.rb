2989fadcbee887a2572835eab4ebc604d0baf41f
sawasawashu <sawasawa01030420@gmail.com>
2023-05-05 16:29:46 +0900

    トークンの値をモデルに追記

--- app/models/purchaser_profile.rb
+++ app/models/purchaser_profile.rb
@@ -1,6 +1,6 @@
 class PurchaserProfile
   include ActiveModel::Model
-  attr_accessor :postal_code, :region_id, :municipality, :address, :building_name, :telephone_number, :user_id, :item_id
+  attr_accessor :postal_code, :region_id, :municipality, :address, :building_name, :telephone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
