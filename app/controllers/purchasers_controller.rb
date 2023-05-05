a01e2e986a7b9edd112c334167862ce34cc24dd7
sawasawashu <sawasawa01030420@gmail.com>
2023-05-05 17:56:38 +0900

    自身が出品した購入ページへの遷移制限

--- app/controllers/purchasers_controller.rb
+++ app/controllers/purchasers_controller.rb
@@ -1,6 +1,7 @@
 class PurchasersController < ApplicationController
 
   before_action :authenticate_user!, only: [:index, :create]
+  before_action :move_to_index, only: [:index,:create]
   
   def index
     @item = Item.find(params[:item_id])
@@ -32,6 +33,11 @@ class PurchasersController < ApplicationController
       card: purchaser_params[:token],
       currency: 'jpy'
     )
+  end
+
+  def move_to_index
+    @item = Item.find(params[:item_id])
+    redirect_to root_path if current_user.id == @item.user_id 
   end
 
 end
