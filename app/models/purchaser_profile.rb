e2c7139172bcfcbbc425ad862f5bfa8126e1a610
sawasawashu <sawasawa01030420@gmail.com>
2023-05-05 16:35:16 +0900

    バリデーションを設定

--- app/models/purchaser_profile.rb
+++ app/models/purchaser_profile.rb
@@ -9,6 +9,7 @@ class PurchaserProfile
     validates :telephone_number, format: { with: /\A\d{10,11}\z/ }
     validates :user_id
     validates :item_id
+    validates :token
   end
   validates :region_id, numericality: { other_than: 1, message: "can't be blank" }
 
