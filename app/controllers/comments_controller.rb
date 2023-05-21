55debc51c8b7a2ea4b7fc4b089803a58b415a2e5
sawasawashu <sawasawa01030420@gmail.com>
2023-05-21 17:16:21 +0900

    メソッドをリファクタリング

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,16 @@
 class CommentsController < ApplicationController
+
+  before_action :set_comment, only: [:edit, :update]
+
   def create
     comment = Comment.create(comment_params)
     redirect_to "/items/#{comment.item.id}"
   end
 
   def edit
-    @item = Item.find(params[:item_id])
-    @comment = Comment.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:item_id])
-    @comment = Comment.find(params[:id])
     if @comment.update(comment_params)
       redirect_to item_path(@item.id)
     else
@@ -23,5 +22,10 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def set_comment
+    @item = Item.find(params[:item_id])
+    @comment = Comment.find(params[:id])
   end
 end
