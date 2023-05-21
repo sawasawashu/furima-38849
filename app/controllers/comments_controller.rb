f36297b57e7267e4aae6cfb610779af1bdf85a78
sawasawashu <sawasawa01030420@gmail.com>
2023-05-21 18:06:26 +0900

    rubocop自動修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: [:edit, :update, :destroy]
-  before_action :move_to_index, only: [:edit,:destroy]
+  before_action :move_to_index, only: [:edit, :destroy]
 
   def create
     comment = Comment.create(comment_params)
