cb8039f55a6c8338b874d8d90168cf329fd4ec89
sawasawashu <sawasawa01030420@gmail.com>
2023-05-28 00:04:26 +0900

    rubocop自動修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,6 +5,5 @@ class Comment < ApplicationRecord
   validates :text, presence: true
   validates :rate, numericality: {
     less_than_or_equal_to: 5, greater_than_or_equal_to: 0.5
-    }, presence: true
-
+  }, presence: true
 end
