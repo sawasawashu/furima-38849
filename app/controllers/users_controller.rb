a1ac1a5bda8cf7af6c605341c4337eafd397cf8e
sawasawashu <sawasawa01030420@gmail.com>
2023-06-03 15:21:46 +0900

    商品を新しい順に表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @nickname = current_user.nickname
-    @items = current_user.items
+    @items = current_user.items.order('created_at DESC')
   end
 
 end
