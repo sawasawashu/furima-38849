acfa4e874cd67da4ac76ca67a16479570b685a40
sawasawashu <sawasawa01030420@gmail.com>
2023-06-03 15:40:11 +0900

    ログインユーザーのみ制限を設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  before_action :authenticate_user!, only: :show
+
   def show
     @nickname = current_user.nickname
     @items = current_user.items.order('created_at DESC')
