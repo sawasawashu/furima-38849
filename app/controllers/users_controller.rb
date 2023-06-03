39be0f9d2fc93f1e120434282155e7c3a8ee69ac
sawasawashu <sawasawa01030420@gmail.com>
2023-06-03 16:22:51 +0900

    商品詳細ページでのマイページリンク設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,9 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, only: :show
 
   def show
-    @nickname = current_user.nickname
-    @items = current_user.items.order('created_at DESC')
+    user =  User.find(params[:id])
+    @nickname = user.nickname
+    @items = user.items.order('created_at DESC')
   end
 
 end
