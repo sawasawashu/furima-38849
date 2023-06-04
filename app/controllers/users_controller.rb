524a70a3f5756eb6b696f6e926a4bf1d9f169f20
sawasawashu <sawasawa01030420@gmail.com>
2023-06-04 15:14:47 +0900

    rubocopによる自動修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,8 @@
 class UsersController < ApplicationController
-
   before_action :authenticate_user!, only: :show
 
   def show
     @user =  User.find(params[:id])
     @items = @user.items.order('created_at DESC')
   end
-
 end
