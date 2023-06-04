833cfca094d1c271fd885cac55f27c7093ddfa45
sawasawashu <sawasawa01030420@gmail.com>
2023-06-04 15:13:59 +0900

    ユーザーの平均点を表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,7 @@ class UsersController < ApplicationController
 
   def show
     @user =  User.find(params[:id])
-    @nickname = @user.nickname
     @items = @user.items.order('created_at DESC')
-    @comment = Comment.new
   end
 
 end
